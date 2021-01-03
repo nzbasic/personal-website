624328a96fb719b972688451b535ca9ebe534408
nzbasic <jcop589@aucklanduni.ac.nz>
2021-01-04 00:27:31 +1300

    initial commit

---
+++ cv/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen bg-gray-700 flex flex-col justify-center">
+      <text className="text-white self-center text-5xl">Coming Soon.</text>
+    </div>
+  );
+}
+
+export default App;
