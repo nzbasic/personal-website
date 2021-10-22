ede88d04ca1ae293e9d706e9305fadb21f510403
nzbasic <jcop589@aucklanduni.ac.nz>
2021-10-22 21:57:24 +1300

    make links more clear

--- src/components/molecules/ProjectExpanded.tsx
+++ src/components/molecules/ProjectExpanded.tsx
@@ -8,9 +8,13 @@ export const ProjectExpanded = ({ data }: { data: ProjectData }) => {
     return (
         <div className="p-2 md:p-4 flex flex-col">
             <span className="mb-2">{data.longDescription}</span>
-            <div className="flex items-center space-x-2">
-                {!data.uni && <a className="mb-4 text-blue-400 text-xl hover:underline font-semibold" href={data.link} target="_blank" rel="noreferrer">Link</a>}
-                <a className="mb-4 text-blue-400 text-xl hover:underline font-semibold" href={data.repo} target="_blank" rel="noreferrer">Repo</a>
+            <div className="flex items-center space-x-2 mb-4">
+                {data.website ? 
+                    <a className="text-blue-400 text-xl hover:underline font-semibold" href={data.link} target="_blank" rel="noreferrer">Website</a> :
+                    <a className="text-blue-400 text-xl hover:underline font-semibold" href={data.link} target="_blank" rel="noreferrer">Release</a>
+                }
+                <span>•</span>
+                <a className=" text-blue-400 text-xl hover:underline font-semibold" href={data.repo} target="_blank" rel="noreferrer">Repo</a>
             </div>
             <span className="text-lg font-semibold">Features:</span>
             <ul className="mb-4">
