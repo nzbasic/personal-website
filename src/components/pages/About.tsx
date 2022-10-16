5531295d072983896e05db02a56cd6228a9aa162
James <54062686+nzbasic@users.noreply.github.com>
2022-10-16 13:24:17 +1300

    update technologies

--- src/components/pages/About.tsx
+++ src/components/pages/About.tsx
@@ -24,12 +24,14 @@ export const About = ({ callback }: { callback: () => void }) => {
                         <img alt="Typescript" src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white" />
                         <img alt="Java" src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white" />
                         <img alt="JavaScript" src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
+                        <img alt="Golang" src="https://img.shields.io/badge/Go-00ADD8?style=for-the-badge&logo=go&logoColor=white" />
                         <img alt="Node.js" src="https://img.shields.io/badge/Node.js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white" />
                         <img alt="React" src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" />
                         <img alt="Angular" src="https://img.shields.io/badge/Angular-DD0031?style=for-the-badge&logo=angular&logoColor=white" />
                         <img alt="Electron" src="https://img.shields.io/badge/Electron-2B2E3A?style=for-the-badge&logo=electron&logoColor=9FEAF9" />
                         <img alt="SQLite" src="https://img.shields.io/badge/SQLite-07405E?style=for-the-badge&logo=sqlite&logoColor=white" />
                         <img alt="MongoDB" src="https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white" />
+                        <img alt="Docker" src="https://img.shields.io/badge/Docker-2CA5E0?style=for-the-badge&logo=docker&logoColor=white" />
                         <img alt="Tailwind CSS" src="https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white" />
                     </div>
 
@@ -37,18 +39,14 @@ export const About = ({ callback }: { callback: () => void }) => {
                     <div className="flex flex-wrap gap-2 mb-4">
                         <img alt="Python" src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" />
                         <img alt="C#" src="https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white" />
-                        <img alt="Golang" src="https://img.shields.io/badge/Go-00ADD8?style=for-the-badge&logo=go&logoColor=white" />
-                        <img alt="Docker" src="https://img.shields.io/badge/Docker-2CA5E0?style=for-the-badge&logo=docker&logoColor=white" />
                         <img alt="Kubernetes" src="https://img.shields.io/badge/kubernetes-326ce5.svg?&style=for-the-badge&logo=kubernetes&logoColor=white" />
                         <img alt="Firebase" src="https://img.shields.io/badge/firebase-ffca28?style=for-the-badge&logo=firebase&logoColor=black" />
                     </div>
 
                     <h2 className="mb-2 text-xl font-semibold">Hobbies</h2>
-                    <p className="mb-1"><span className="font-semibold mr-1">osu!:</span>A super cool rhythm game with a very active community, especially in NZ. Nothing better than chilling and listening to some songs while tapping along. What makes the game complete is the public API that I have made heavy use of for my projects which has been a catalyst for a lot of learning.</p>
+                    <p className="mb-1"><span className="font-semibold mr-1">osu!:</span>A rhythm game with a very active community, especially in NZ. What makes the game amazing is the public API that I have made heavy use of for my projects which has been a catalyst for a lot of learning.</p>
                     <p className="mb-1"><span className="font-semibold mr-1">Projects:</span>My software projects are some of the only things that I can just sink into and forget about everything else.</p>
                     <p className="mb-1"><span className="font-semibold mr-1">Keyboards:</span>I am a serial collector/hoarder (a.k.a bad with money 🤭) and most of that energy goes into building keyboards. You can probably find me chatting in keyboard discords or at meetups!</p>
-                    <p className="mb-4"><span className="font-semibold mr-1">Fitness:</span>I love hitting the gym 💪, I needed a better way to be active so I started going in 2020 and try to go every day! (Unless COVID lockdown 😞)</p>
-
                     <h2 className="mb-2 text-xl font-semibold">Socials</h2>
                     <div className="flex space-x-2">
                         <LinkedInIcon className="cursor-pointer" onClick={() => window.open("https://linkedin.com/in/jamescoppard024")} />
